c5b9b44e531129cb25e8863ac015a72548776de7
kkedzierski <k.kedzierski@global4net.com>
2024-11-02 15:06:55 +0100

    #main: added docker configuration

---
+++ docker/etc/php/main.Dockerfile
@@ -0,0 +1,35 @@
+FROM php:8.2-fpm
+
+# Install dependencies
+# -q flag is used to reduce the output of apt-get, is stands for quiet
+# -y flag is used to automatically answer yes to prompts is stands for yes
+# xdebug is required for debugging
+# pdo is required for database connections
+# pdo_mysql is required for mysql connections
+# libzip-dev is required for zip extension
+# libicu-dev is required for intl extension
+# zip is required for composer
+# git is requried for grumphp
+RUN apt-get -q update && apt-get -qy install \
+    zip \
+    cron \
+    libzip-dev \
+    libicu-dev \
+    git \
+    && pecl install pcov \
+    && docker-php-ext-enable pcov \
+    && docker-php-ext-install intl opcache pdo pdo_mysql \
+    && pecl install xdebug \
+    && docker-php-ext-enable xdebug
+
+WORKDIR /var/www/html
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+COPY . /var/www/html
+
+# Copy configuration files
+COPY server/.php-cs-fixer.php /var/www/html/.php-cs-fixer.php
+COPY server/phpstan.neon /var/www/html/phpstan.neon
+
+RUN ./vendor/bin/grumphp git:init || true
---
+++ docker/run-dashboard.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+environment=dev
+rebuilt=false
+
+source ./docker/.env
+
+while getopts e:r: flag
+do
+    case "${flag}" in
+        e) environment=${OPTARG};;
+        r) rebuilt=${OPTARG};;
+    esac
+done
+
+if [ "$rebuilt" == "true" ]; then
+  echo "Rebuilding configuration image..."
+  docker build --no-cache . -f ./docker/etc/php/main.Dockerfile
+fi
+
+ENV="./docker/.env.dist"
+if [ -f ./docker/.env ]; then
+    ENV="./docker/.env"
+fi
+
+BASE_DIRECTORY=$(pwd)/server
+
+echo "Building and starting containers..."
+docker-compose down
+docker-compose --env-file $ENV up -d --build
+echo "Containers are up and running."
+
+echo "Installing composer dependencies..."
+docker exec -it "${CONTAINER_NAME}" composer install
+echo "Composer dependencies installed."
+
+echo "Creating database schema..."
+docker exec -it "${CONTAINER_NAME}" bin/console doctrine:database:create --if-not-exists
+docker exec -it "${CONTAINER_NAME}" bin/console doctrine:schema:update --force
+docker exec -it "${CONTAINER_NAME}" bin/console doctrine:schema:validate
+
+echo "Creating test database schema..."
+docker exec -it "${CONTAINER_NAME}" bin/console doctrine:schema:update -etest --force
+docker exec -it "${CONTAINER_NAME}" bin/console doctrine:schema:update -etest --force
+docker exec -it "${CONTAINER_NAME}" bin/console doctrine:schema:validate -etest
